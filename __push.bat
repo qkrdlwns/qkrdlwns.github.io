8099a751ee0f1e4f1a3c8a764088f0a7bc7e8a5f
fakeclear <59392071+qkrdlwns@users.noreply.github.com>
2025-08-09 11:33:01 +0900

    commit blog

---
+++ __push.bat
@@ -0,0 +1,18 @@
+cd ./../
+cd ./obsidian-vault
+git rm -r --cached . 
+git add .
+git commit -m "commit blog"
+git push
+cd ./obsidian-image
+git rm -r --cached . 
+git add .
+git commit -m "commit blog"
+git push
+cd ./../../
+cd ./qkrdlwns.github.io
+git rm -r --cached . 
+git add .
+git commit -m "commit blog"
+git push
+cd ./../
